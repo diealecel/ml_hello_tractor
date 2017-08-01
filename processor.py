81bfacceac5095b94ec4c09dc18ce7744e6c72b4
diealecel <diealecel@gmail.com>
2017-08-01 10:59:58 -0700

    formatting

---
+++ processor.py
@@ -0,0 +1,21 @@
+import csv
+
+def load_CSV(filename):
+    matrix = []
+
+    with open(filename) as f:
+        reader = csv.reader(f)
+
+        for row in reader:
+            string_row = []
+
+            for value in row:
+                string_row.append(value)
+
+            matrix.append(string_row)
+
+    return matrix
+
+
+if __name__ == '__main__':
+    pass
